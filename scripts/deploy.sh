281c56fb1f3efb307aede22a2a679eea8a835861
Ari Kim <kari300084@gmail.com>
2024-10-22 20:45:40 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,27 +3,28 @@
 cd /home/ubuntu/app
 
 DOCKER_APP_NAME=spring
+LOG_FILE="deploy.log"
 
 # 실행중인 blue가 있는지
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
 
 # green이 실행중이면 blue up
 if [ -z "$EXIST_BLUE" ]; then
-	echo "blue up"
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+	echo "$(date): blue up" >> $LOG_FILE
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build >> $LOG_FILE 2>&1
 
 	sleep 30
 
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-	docker image prune -af # 사용하지 않는 이미지 삭제
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down >> $LOG_FILE 2>&1
+	docker image prune -af >> $LOG_FILE 2>&1
 
 # blue가 실행중이면 green up
 else
-	echo "green up"
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+	echo "$(date): green up" >> $LOG_FILE
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build >> $LOG_FILE 2>&1
 
 	sleep 30
 
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-	docker image prune -af
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down >> $LOG_FILE 2>&1
+	docker image prune -af >> $LOG_FILE 2>&1
 fi
